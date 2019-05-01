d4142ac619390f76ba739a153d9482b16975fe31
springrat <32113209+springrat@users.noreply.github.com>
2019-05-01 16:08:06 -0400

    Example Module 1
    Using math module and calling on files within 'math' package to convert Degrees into radians.

---
+++ mathmod1.py
@@ -0,0 +1,17 @@
+#call on top module
+import math
+
+#call on modules 1 and 2
+from math import radians
+
+#prompt for input
+x = int(input('To Convert Radians to Degrees, Please Enter Degrees: '))
+
+
+print('Radians:')
+
+#math.radians are trig functions
+#math.radians converts the degrees into radians
+
+
+print(math.radians(x))
